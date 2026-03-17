fadb96c28ad58490d5309b8376207c291d658592
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2026-03-17 10:52:06 +0100

    chore: removed useless boilerplate code

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [LinksModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [DatabaseModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
